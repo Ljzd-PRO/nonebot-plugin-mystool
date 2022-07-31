41fdbe01b581b09dd92a9a0cea1e3e474077daa4
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2022-07-31 23:42:27 +0800

    删除不会使用到的函数

--- utils.py
+++ utils.py
@@ -1,6 +1,3 @@
-import httpx
-import asyncio
-import requests
 import random
 import string
 
@@ -26,60 +23,3 @@ def generateDeviceID() -> str:
                                                                                                                      4)).lower() + "-" + "".join(random.sample(string.ascii_letters + string.digits,
                                                                                                                                                                4)).lower() + "-" + "".join(random.sample(string.ascii_letters + string.digits,
                                                                                                                                                                                                          12)).lower()
-
-
-async def onekeyCookie(phone, captcha) -> None:
-    login_1_headers = {
-        "Host": "webapi.account.mihoyo.com",
-        "Connection": "keep-alive",
-        "Content-Length": "79",
-        "sec-ch-ua": UA,
-        "DNT": "1",
-        "x-rpc-device_model": X_RPC_DEVICE_MODEL,
-        "sec-ch-ua-mobile": "?0",
-        "User-Agent": USER_AGENT_PC,
-        "x-rpc-device_id": generateDeviceID(),
-        "Accept": "application/json, text/plain, */*",
-        "x-rpc-device_name": X_RPC_DEVICE_NAME,
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "x-rpc-client_type": "4",
-        "sec-ch-ua-platform": "\"macOS\"",
-        "Origin": "https://user.mihoyo.com",
-        "Sec-Fetch-Site": "same-site",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Dest": "empty",
-        "Referer": "https://user.mihoyo.com/",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
-    }
-
-    try:
-        login_1_req = httpx.post(
-            "https://webapi.account.mihoyo.com/Api/login_by_mobilecaptcha", headers=login_1_headers, data="mobile={0}&mobile_captcha={1}&source=user.mihoyo.com".format(phone, captcha))
-    except:
-        return
-    login_1_cookie = requests.utils.dict_from_cookiejar(
-        login_1_req.cookies)
-
-    if "login_ticket" not in login_1_cookie:
-        print("> 由于Cookie缺少login_ticket，无法继续，回车以返回\n")
-        return
-
-    for cookie in ("login_uid", "stuid", "ltuid", "account_id"):
-        if cookie in login_1_cookie:
-            bbs_uid = login_1_cookie[cookie]
-            break
-    if bbs_uid == None:
-        print("> 由于Cookie缺少uid，无法继续，回车以返回\n")
-        return
-
-    print("正在获取stoken...")
-    try:
-        get_stoken_req = httpx.get(
-            "https://api-takumi.mihoyo.com/auth/api/getMultiTokenByLoginTicket?login_ticket={0}&token_types=3&uid={1}".format(login_1_cookie["login_ticket"], bbs_uid))
-        stoken = list(filter(
-            lambda data: data["name"] == "stoken", get_stoken_req.json()["data"]["list"]))[0]["token"]
-        exit(1)
-    except:
-        print("> 获取stoken失败，一种可能是登录失效，回车以返回\n")
-        return
