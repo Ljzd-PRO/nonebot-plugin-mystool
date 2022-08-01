fd0ad0b7fb5d2eb7c9dda127c89fe8d699630270
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2022-08-01 13:46:46 +0800

    增加Cookie str和dict互转的方法

--- utils.py
+++ utils.py
@@ -23,3 +23,32 @@ def generateDeviceID() -> str:
                                                                                                                      4)).lower() + "-" + "".join(random.sample(string.ascii_letters + string.digits,
                                                                                                                                                                4)).lower() + "-" + "".join(random.sample(string.ascii_letters + string.digits,
                                                                                                                                                                                                          12)).lower()
+
+
+def cookie_str_to_dict(cookie_str: str) -> dict[str, str]:
+    """
+    将字符串Cookie转换为字典Cookie
+    """
+    cookie_str = cookie_str.replace(" ", "")
+    # Cookie末尾缺少 ; 的情况
+    if cookie_str[-1] != ";":
+        cookie_str += ";"
+
+    cookie_dict = {}
+    start = 0
+    while start != len(cookie_str):
+        mid = cookie_str.find("=", start)
+        end = cookie_str.find(";", mid)
+        cookie_dict.setdefault(cookie_str[start:mid], cookie_str[mid + 1:end])
+        start = end + 1
+    return cookie_dict
+
+
+def cookie_dict_to_str(cookie_dict: dict[str, str]) -> str:
+    """
+    将字符串Cookie转换为字典Cookie
+    """
+    cookie_str = ""
+    for key in cookie_dict:
+        cookie_str += (key + "=" + cookie_dict[key] + ";")
+    return cookie_str
