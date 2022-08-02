63647a76755ccdd046866d94ea70b4b7deead97a
Ljzd-PRO <mcdhacg@foxmail.com>
2022-08-02 12:27:06 +0800

    去除USER_AGENT_MOBILE等全局变量，增加PATH目录绝对路径获取

--- mysTool/utils.py
+++ mysTool/utils.py
@@ -1,17 +1,8 @@
 import random
 import string
+from pathlib import Path
 
-USER_AGENT_MOBILE = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/2.25.1"
-"""移动端 User-Agent"""
-USER_AGENT_PC = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15"
-"""桌面端 User-Agent"""
-X_RPC_DEVICE_MODEL = "OS X 10.15.7"
-"""Headers所用的 x-rpc-device_model"""
-X_RPC_DEVICE_NAME = "Microsoft Edge 103.0.1264.62"
-"""Headers所用的 x-rpc-device_name"""
-UA = "\".Not/A)Brand\";v=\"99\", \"Microsoft Edge\";v=\"103\", \"Chromium\";v=\"103\""
-"""Headers所用的 sec-ch-ua"""
-
+PATH = Path(__file__).parent.absolute()
 
 def generateDeviceID() -> str:
     """
