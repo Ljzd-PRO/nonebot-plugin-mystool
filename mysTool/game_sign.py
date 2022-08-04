2dba241a879ec4afd2d410ab4af1736229cb41ac
Ljzd-PRO <mcdhacg@foxmail.com>
2022-08-04 16:24:52 +0800

    增加获取签到记录

--- mysTool/game_sign.py
+++ mysTool/game_sign.py
@@ -4,15 +4,21 @@ from nonebot.log import logger
 from .config import mysTool_config as conf
 from .utils import *
 from .data import UserAccount
+from typing import Literal
 
 ACT_ID = {
     "ys": "e202009291139501"
 }
 URL_REWARD = {
-    "ys": "https://api-takumi.mihoyo.com/event/bbs_sign_reward/home?act_id={}".format(ACT_ID["ys"])
+    "ys": {
+        "reward": "https://api-takumi.mihoyo.com/event/bbs_sign_reward/home?act_id={}".format(ACT_ID["ys"]),
+        "info": "https://api-takumi.mihoyo.com/event/bbs_sign_reward/info?act_id={actID}&region={region}&uid={uid}",
+        "sign": "https://api-takumi.mihoyo.com/event/bbs_sign_reward/sign"
+    }
 }
 URL_GAME_ROLE = "https://api-takumi.mihoyo.com/binding/api/getUserGameRoles?point_sn=myb&action_ticket={actionTicket}&game_biz={game_biz}"
 URL_SIGN = "https://api-takumi.mihoyo.com/event/bbs_sign_reward/sign"
+
 HEADERS_REWARD = {
     "Host": "api-takumi.mihoyo.com",
     "Origin": "https://webstatic.mihoyo.com",
@@ -30,12 +36,10 @@ HEADERS_OTHER = {
     "Referer": "https://webstatic.mihoyo.com/",
     "x-rpc-device_name": conf.device.X_RPC_DEVICE_NAME_MOBILE,
     "Origin": "https://webstatic.mihoyo.com",
-    "Content-Length": "66",
     "Connection": "keep-alive",
     "x-rpc-channel": conf.device.X_RPC_CHANNEL,
     "x-rpc-app_version": conf.device.X_RPC_APP_VERSION,
     "Accept-Language": "zh-CN,zh-Hans;q=0.9",
-    "DS": None,
     "x-rpc-device_id": None,
     "x-rpc-client_type": "5",
     "Accept": "application/json, text/plain, */*",
@@ -45,8 +49,12 @@ HEADERS_OTHER = {
     "x-rpc-platform": conf.device.X_RPC_PLATFORM
 }
 
+
 class Award:
-    def __init__(self, awards_dict:dict) -> None:
+    """
+    签到奖励数据
+    """
+    def __init__(self, awards_dict: dict) -> None:
         self.awards_dict = awards_dict
 
     @property
@@ -61,23 +69,84 @@ class Award:
     def name(self) -> int:
         return self.awards_dict["cnt"]
 
+
+class Info:
+    """
+    签到记录数据
+    """
+    def __init__(self, info_dict: dict) -> None:
+        self.info_dict = info_dict
+
+    @property
+    def isSign(self) -> bool:
+        """
+        今日是否已经签到
+        """
+        return self.info_dict["is_sign"]
+
+    @property
+    def firstBind(self) -> bool:
+        """
+        是否是第一次绑定
+        """
+        return self.info_dict["first_bind"]
+
+    @property
+    def monthFirst(self) -> bool:
+        """
+        是否是月初第一天
+        """
+        return self.info_dict["month_first"]
+
+    @property
+    def totalDays(self) -> int:
+        """
+        已签多少天
+        """
+        return self.info_dict["total_sign_day"]
+
+    @property
+    def missedDays(self) -> int:
+        """
+        漏签多少天
+        """
+        return self.info_dict["sign_cnt_missed"]
+
+
 class Sign:
-
-
+    """
+    签到相关(需先初始化对象)
+    """
     def __init__(self, account: UserAccount) -> None:
         self.cookie = account.cookie
         self.deviceID = account.deviceID
 
-    async def reward(self, game:str):
-        res: httpx.Response = await httpx.get(URL_REWARD[game], headers=HEADERS_REWARD)
+    async def reward(self, game: Literal["ys"]):
+        """
+        获取签到奖励信息
+        """
+        res: httpx.Response = await httpx.get(URL_REWARD[game]["reward"], headers=HEADERS_REWARD)
         try:
             return Award(res.json()["data"]["awards"])
         except KeyError:
-            print("服务器没有正确返回")
+            logger.error(conf.LOG_HEAD + "获取签到奖励信息 - 服务器没有正确返回")
             logger.debug(conf.LOG_HEAD + traceback.format_exc())
         except:
-            print("网络连接失败")
+            logger.error(conf.LOG_HEAD + "获取签到奖励信息 - 请求失败")
             logger.debug(conf.LOG_HEAD + traceback.format_exc())
 
-    async def info(game:str):
-        ...
+    async def info(account: UserAccount, game: Literal["ys"]):
+        """
+        获取签到记录
+        """
+        headers = HEADERS_OTHER.copy()
+        headers["x-rpc-device_id"] = account.deviceID
+        res: httpx.Response = await httpx.get(URL_REWARD[game]["info"], headers=headers, cookies=account.cookie)
+        try:
+            return Info(res.json()["data"])
+        except KeyError:
+            logger.error(conf.LOG_HEAD + "获取签到记录 - 服务器没有正确返回")
+            logger.debug(conf.LOG_HEAD + traceback.format_exc())
+        except:
+            logger.error(conf.LOG_HEAD + "获取签到记录 - 请求失败")
+            logger.debug(conf.LOG_HEAD + traceback.format_exc())
