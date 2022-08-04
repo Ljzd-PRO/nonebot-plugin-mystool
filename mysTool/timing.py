181ebfb9ed8c63462e659cb8e5f98c1bfbe97125
Ljzd-PRO <63289359+Ljzd-PRO@users.noreply.github.com>
2022-08-05 03:17:34 +0800

    Update timing.py

--- mysTool/timing.py
+++ mysTool/timing.py
@@ -12,23 +12,35 @@ from game_sign import *
 
 
 sign_timing = require("nonebot_plugin_apscheduler").scheduler
-@sign_timing.scheduled_job("cron", hour='0', minute='00', id="daily_sign") #此处应改为可由config读入签到时间
-async def daily_sign():
-    await ... #签到函数
-    await ... #向订阅者发送每日签到播报
 
-manually_sign = on_command('sign', aliases={'签到', '手动签到'}, permission=SUPERUSER, priority=4, block=True)
+
+# 此处应改为可由config读入签到时间
+@sign_timing.scheduled_job("cron", hour='0', minute='00', id="daily_sign")
+async def daily_sign():
+    await ...  # 签到函数
+    await ...  # 向订阅者发送每日签到播报
+
+manually_sign = on_command(
+    'sign', aliases={'签到', '手动签到'}, permission=SUPERUSER, priority=4, block=True)
+
+
 @manually_sign.handle()
 async def _(event: PrivateMessageEvent, state: T_State):
-    await ... #签到函数
+    await ...  # 签到函数
     await manually_sign.send('已完成签到')
 
 update_timing = require("nonebot_plugin_apscheduler").scheduler
-@update_timing.scheduled_job("cron", hour='0', minute='00', id="daily_update") #此处应改为可由config读入更新时间
-async def daily_update():
-    await ... #米游社商品更新函数
 
-manually_update = on_command('update_good', aliases={'商品更新', '米游社商品更新'}, permission=SUPERUSER, priority=4, block=True)
+
+# 此处应改为可由config读入更新时间
+@update_timing.scheduled_job("cron", hour='0', minute='00', id="daily_update")
+async def daily_update():
+    await ...  # 米游社商品更新函数
+
+manually_update = on_command('update_good', aliases={
+                             '商品更新', '米游社商品更新'}, permission=SUPERUSER, priority=4, block=True)
+
+
 async def _(event: PrivateMessageEvent, state: T_State):
-    await ... #米游社商品更新函数
+    await ...  # 米游社商品更新函数
     await manually_sign.send('已完成商品更新')
