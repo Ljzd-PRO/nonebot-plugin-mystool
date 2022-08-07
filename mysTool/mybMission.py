8e7c21df7f6da79b0b0853438eabf2e026cef8cc
Ljzd-PRO <mcdhacg@foxmail.com>
2022-08-07 14:59:09 +0800

    增加阅读、点赞

--- mysTool/mybMission.py
+++ mysTool/mybMission.py
@@ -1,13 +1,15 @@
+from email import header
 import httpx
 from .config import mysTool_config as conf
 from .data import UserAccount
 from .utils import generateDS
-from typing import Literal
+from typing import Literal, Union
 from asyncio import sleep
 
-URL_SIGN = "https://api-takumi.mihoyo.com/apihub/sapi/signIn"
-URL_GET_POST = "https://api-takumi.mihoyo.com/post/api/getForumPostList?forum_id={}&is_good=false&is_hot=false&page_size=20&sort=create"
-URL_READ = "https://api-takumi.mihoyo.com/post/api/getPostFull?post_id={}"
+URL_SIGN = "https://bbs-api.mihoyo.com/apihub/app/api/signIn"
+URL_GET_POST = "https://bbs-api.mihoyo.com/apihub/app/api/getForumPostList?forum_id={}&is_good=false&is_hot=false&page_size=20&sort=create"
+URL_READ = "https://bbs-api.mihoyo.com/apihub/app/api/getPostFull?post_id={}"
+URL_LIKE = "https://api-takumi.mihoyo.com/apihub/sapi/upvotePost"
 HEADERS = {
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "zh-cn",
@@ -53,23 +55,68 @@ class Mission:
         self.cookie = {"stuid": account.bbsUID,
                        "stoken": account.cookie["stoken"]}
         self.client = httpx.AsyncClient(cookies=self.cookie)
+        self.mybNum: int = 0
 
     async def sign(self, game: Literal["bh3", "ys", "bh2", "wd", "xq"]):
         headers = HEADERS.copy()
         headers.setdefault("DS", generateDS())
         res = await self.client.post(URL_SIGN, headers=headers, json={"gids": GAME_ID[game]["gids"]})
-        ...
+        try:
+            self.mybNum = res.json()["data"]["points"]
+            return True
+        except KeyError:
+            return False
 
-    async def read(self, game: Literal["bh3", "ys", "bh2", "wd", "xq"], times: int = 3):
+    async def get_posts(self, game: Literal["bh3", "ys", "bh2", "wd", "xq"]) -> Union[list[str], None]:
         headers = HEADERS.copy()
         headers.setdefault("DS", generateDS())
-        res_getPost = await self.client.get(URL_GET_POST.format(GAME_ID[game]["fid"]), headers=headers)
+        res = await self.client.get(URL_GET_POST.format(GAME_ID[game]["fid"]), headers=headers)
+        postID_list = []
+        try:
+            data = res.json()["data"]["list"]
+            for post in data:
+                if post["self_operation"]["attitude"] == 0:
+                    postID_list.append(post['post']['post_id'])
+        except KeyError:
+            return None
+        return postID_list
+
+    async def read(self, game: Literal["bh3", "ys", "bh2", "wd", "xq"], readTimes: int = 3, likeTimes: int = 10):
+        headers = HEADERS.copy()
+
         count = 0
-        while count < times:
-            postID = res_getPost['data']['list'][count]['post']['post_id']
+        postID_list = await self.get_posts(game)
+        while count < readTimes:
             await sleep(conf.SLEEP_TIME)
             headers["DS"] = generateDS()
-            res = await ...
+            for postID in postID_list:
+                if count == readTimes:
+                    break
+                res = await self.client.get(URL_READ.format(postID), headers=headers)
+                try:
+                    "self_operation" in res.json()["data"]["post"]
+                    count += 1
+                except KeyError:
+                    ...
+            postID_list = await self.get_posts(game)
+
+        count = 0
+        while count < likeTimes:
+            await sleep(conf.SLEEP_TIME)
+            headers["DS"] = generateDS()
+            for postID in postID_list:
+                if count == likeTimes:
+                    break
+                res = await self.client.post(URL_LIKE, headers=headers, json={'is_cancel': False,  'post_id': postID})
+                try:
+                    if ...:
+                        ...
+                    count += 1
+                except KeyError:
+                    ...
+            postID_list = await self.get_posts(game)
+
+        return True
 
     async def __del__(self):
         await self.client.aclose()
