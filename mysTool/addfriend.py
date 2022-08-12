c774f3e88e1f0151dc5a3e31a8bf7ebfcc8b4e4a
Everything0519 <82890373+Everything0519@users.noreply.github.com>
2022-08-12 21:35:57 +0800

    加好友时自动通过并打招呼模块

---
+++ mysTool/addfriend.py
@@ -0,0 +1,21 @@
+from nonebot.adapters.onebot.v11 import Bot, RequestEvent, FriendRequestEvent,GroupRequestEvent
+from nonebot import get_driver
+from nonebot import on_request
+import asyncio
+
+FriendRequest = on_request(priority=1, block=True)
+@FriendRequest.handle()
+async def _(bot: Bot, event: RequestEvent):
+    command = list(get_driver().config.command_start)[0]
+    if isinstance(event, FriendRequestEvent):
+        await bot.set_friend_add_request(flag=event.flag, approve=True)
+        await asyncio.sleep(1.5)
+        await bot.send_private_msg(user_id=event.user_id, message=f'欢迎使用米游社小助手，请发送{command}help查看更多用法哦~')
+    elif isinstance(event, GroupRequestEvent):
+        if event.sub_type!='invite':
+            print(event.sub_type)
+            return
+        else:
+            await asyncio.sleep(1.5)
+            await bot.send_group_msg(group_id=event.group_id, message=f'欢迎使用米游社小助手，请添加小助手为好友后，发送{command}help查看更多用法哦~')
+        
