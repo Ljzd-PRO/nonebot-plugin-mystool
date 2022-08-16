f8e49ba09aefb99b929b46d7324f4109a640ec7b
Everything0519 <82890373+Everything0519@users.noreply.github.com>
2022-08-16 22:34:16 +0800

    进一步完善兑换前端

--- mysTool/myb_exchange.py
+++ mysTool/myb_exchange.py
@@ -3,12 +3,12 @@
 """
 from nonebot import on_command, get_driver
 from nonebot.matcher import Matcher
-from nonebot.params import CommandArg, Arg, ArgPlainText
-from nonebot.adapters.onebot.v11 import PrivateMessageEvent, MessageEvent, MessageSegment
+from nonebot.params import CommandArg, Arg, ArgPlainText, T_State
+from nonebot.adapters.onebot.v11 import Bot, PrivateMessageEvent, MessageEvent, MessageSegment
 from nonebot.adapters.onebot.v11.message import Message
 from .config import mysTool_config as conf
 from .data import UserData
-from .exchange import get_good_list
+from .exchange import *
 
 __cs = ''
 if conf.USE_COMMAND_START:
@@ -27,7 +27,7 @@ myb_exchange_plan.__help_msg__ = f"""\
 """.strip()
 
 @myb_exchange_plan.handle()
-async def handle_first_receive(event: PrivateMessageEvent, matcher: Matcher, args: Message = CommandArg()):
+async def handle_first_receive(event: PrivateMessageEvent, matcher: Matcher, state: T_State, args: Message = CommandArg()):
     qq = event.user_id
     accounts = UserData.read_account_all(qq)
     if accounts:
@@ -41,6 +41,7 @@ async def handle_first_receive(event: PrivateMessageEvent, matcher: Matcher, arg
             account = UserData.read_account(qq, phone)
             if not account:
                 await matcher.reject('您输入的手机号有误，请重新输入')
+    state['account'] = account
     msg = ... # data内查看兑换计划函数
     if args:
         matcher.set_arg("content", args)
@@ -48,7 +49,9 @@ async def handle_first_receive(event: PrivateMessageEvent, matcher: Matcher, arg
         await matcher.finish(msg+matcher.__help_msg__)
 
 @myb_exchange_plan.got('content')
-async def _(event: PrivateMessageEvent, matcher: Matcher, content: Message = Arg()):
+async def _(event: PrivateMessageEvent, matcher: Matcher, state: T_State, bot: Bot, content: Message = Arg()):
+    account = state['T-state']
+    qq = event.user_id
     arg = content.extract_plain_text().strip()
     good_list = get_good_list('bh3') + get_good_list('ys') + get_good_list('bh2') + get_good_list('wd') + get_good_list('bbs')
     Flag = True
@@ -61,7 +64,11 @@ async def _(event: PrivateMessageEvent, matcher: Matcher, content: Message = Arg
     if arg[0] == '+':
         if good.time:
             ... # 写入兑换计划
-            ... # 设置兑换计划
+            exchange_plan = Exchange(account, good.goodID)
+            if exchange_plan.result == -1:
+                await bot.send_private_msg(user_id=qq, message="商品初始化失败，请重新尝试")
+            else:
+                ...
             await matcher.finish(f'设置兑换计划成功！将于{good.time}开始兑换，兑换结果会实时通知您')
         else:
             await matcher.finish(f'该商品暂时不可以兑换，请重新设置')
@@ -71,6 +78,16 @@ async def _(event: PrivateMessageEvent, matcher: Matcher, content: Message = Arg
         await matcher.finish('兑换计划删除成功')
     else:
         matcher.finish('您的输入有误，请重新输入')
+
+async def exchange(exchange_plan: Exchange, bot: Bot, qq: str):
+    result = exchange_plan.start()
+    if result[0]:
+        await bot.send_private_msg(user_id=qq, message=f"商品{exchange_plan.goodID}兑换成功，可前往米游社查看")
+    else:
+        await bot.send_private_msg(user_id=qq, message=f"商品{exchange_plan.goodID}兑换失败\n{result[1]}")
+
+
+
 
 get_good_image = on_command(
     __cs+'商品列表', aliases={__cs+'商品图片', __cs+'米游社商品列表', __cs+'米游币商品图片'}, priority=4, block=True)
