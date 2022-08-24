4eef922229d5debb512e7b0adc23532ac6e00179
Ljzd-PRO <mcdhacg@foxmail.com>
2022-08-24 22:06:32 +0800

    增加重试机制

--- mysTool/gameSign.py
+++ mysTool/gameSign.py
@@ -5,12 +5,13 @@ import traceback
 from typing import List, Literal
 
 import httpx
+import tenacity
 from nonebot.log import logger
 
 from .bbsAPI import GameInfo, GameRecord, get_game_record
 from .config import mysTool_config as conf
 from .data import UserAccount
-from .utils import check_login, generateDS
+from .utils import check_login, custom_attempt_times, generateDS
 
 ACT_ID = {
     "ys": "e202009291139501",
@@ -150,17 +151,23 @@ class GameSign:
         self.account = account
         self.signResult: dict = None
 
-    async def reward(self, game: Literal["ys", "bh3"]):
+    async def reward(self, game: Literal["ys", "bh3"], retry: bool = True):
         """
         获取签到奖励信息，若返回`None`说明失败
+
+        参数:
+            `game`: 目标游戏缩写
+            `retry`: 是否允许重试
         """
-        async with httpx.AsyncClient() as client:
-            res = await client.get(URLS[game]["reward"], headers=HEADERS_REWARD, timeout=conf.TIME_OUT)
         try:
-            award_list: List[Award] = []
-            for award in res.json()["data"]["awards"]:
-                award_list.append(Award(award))
-            return award_list
+            async for attempt in tenacity.AsyncRetrying(stop=custom_attempt_times(retry), reraise=True):
+                with attempt:
+                    async with httpx.AsyncClient() as client:
+                        res = await client.get(URLS[game]["reward"], headers=HEADERS_REWARD, timeout=conf.TIME_OUT)
+                    award_list: List[Award] = []
+                    for award in res.json()["data"]["awards"]:
+                        award_list.append(Award(award))
+                    return award_list
         except KeyError:
             logger.error(conf.LOG_HEAD + "获取签到奖励信息 - 服务器没有正确返回")
             logger.debug(conf.LOG_HEAD + "网络请求返回: {}".format(res.text))
@@ -169,9 +176,15 @@ class GameSign:
             logger.error(conf.LOG_HEAD + "获取签到奖励信息 - 请求失败")
             logger.debug(conf.LOG_HEAD + traceback.format_exc())
 
-    async def info(self, game: Literal["ys", "bh3"], gameUID: str, region: str = None):
+    async def info(self, game: Literal["ys", "bh3"], gameUID: str, region: str = None, retry: bool = True):
         """
         获取签到记录
+
+        参数:
+            `game`: 目标游戏缩写
+            `gameUID`: 用户游戏UID
+            `region`: 用户游戏区服(若为`None`将会自动获取)
+            `retry`: 是否允许重试
 
         - 若返回 `-1` 说明用户登录失效
         - 若返回 `-2` 说明服务器没有正确返回
@@ -197,15 +210,18 @@ class GameSign:
             return -4
 
         headers["DS"] = generateDS()
-        async with httpx.AsyncClient() as client:
-            res = await client.get(URLS[game]["info"].format(region=region, uid=gameUID), headers=headers, cookies=self.cookie, timeout=conf.TIME_OUT)
-        if not check_login(res.text):
-            logger.info(
-                conf.LOG_HEAD + "获取签到记录 - 用户 {} 登录失效".format(self.account.phone))
-            logger.debug(conf.LOG_HEAD + "网络请求返回: {}".format(res.text))
-            return -1
         try:
-            return Info(res.json()["data"])
+            async for attempt in tenacity.AsyncRetrying(stop=custom_attempt_times(retry), reraise=True):
+                with attempt:
+                    async with httpx.AsyncClient() as client:
+                        res = await client.get(URLS[game]["info"].format(region=region, uid=gameUID), headers=headers, cookies=self.cookie, timeout=conf.TIME_OUT)
+                    if not check_login(res.text):
+                        logger.info(
+                            conf.LOG_HEAD + "获取签到记录 - 用户 {} 登录失效".format(self.account.phone))
+                        logger.debug(conf.LOG_HEAD +
+                                     "网络请求返回: {}".format(res.text))
+                        return -1
+                    return Info(res.json()["data"])
         except KeyError:
             logger.error(conf.LOG_HEAD + "获取签到记录 - 服务器没有正确返回")
             logger.debug(conf.LOG_HEAD + "网络请求返回: {}".format(res.text))
@@ -216,9 +232,14 @@ class GameSign:
             logger.debug(conf.LOG_HEAD + traceback.format_exc())
             return -3
 
-    async def sign(self, game: Literal["ys", "bh3"], gameUID: str):
+    async def sign(self, game: Literal["ys", "bh3"], gameUID: str, retry: bool = True):
         """
         签到
+
+        参数:
+            `game`: 目标游戏缩写
+            `gameUID`: 用户游戏UID
+            `retry`: 是否允许重试
 
         - 若签到成功，返回 `True`
         - 若返回 `-1` 说明用户登录失效
@@ -240,19 +261,22 @@ class GameSign:
             "region": region,
             "uid": gameUID
         }
-        async with httpx.AsyncClient() as client:
-            res = await client.post(URLS[game]["sign"], headers=headers, cookies=self.cookie, timeout=conf.TIME_OUT, json=data)
-        if not check_login(res.text):
-            logger.info(
-                conf.LOG_HEAD + "签到 - 用户 {} 登录失效".format(self.account.phone))
-            logger.debug(conf.LOG_HEAD + "网络请求返回: {}".format(res.text))
-            return -1
         try:
-            self.signResult = res.json()
-            if (game == "ys" and self.signResult["data"]["success"] == 0) or (game == "bh3" and self.signResult["data"]["message"] == ""):
-                return True
-            else:
-                return -4
+            async for attempt in tenacity.AsyncRetrying(stop=custom_attempt_times(retry), reraise=True):
+                with attempt:
+                    async with httpx.AsyncClient() as client:
+                        res = await client.post(URLS[game]["sign"], headers=headers, cookies=self.cookie, timeout=conf.TIME_OUT, json=data)
+                    if not check_login(res.text):
+                        logger.info(
+                            conf.LOG_HEAD + "签到 - 用户 {} 登录失效".format(self.account.phone))
+                        logger.debug(conf.LOG_HEAD +
+                                     "网络请求返回: {}".format(res.text))
+                        return -1
+                    self.signResult = res.json()
+                    if (game == "ys" and self.signResult["data"]["success"] == 0) or (game == "bh3" and self.signResult["data"]["message"] == ""):
+                        return True
+                    else:
+                        return -4
         except KeyError:
             logger.error(conf.LOG_HEAD + "签到 - 服务器没有正确返回")
             logger.debug(conf.LOG_HEAD + "网络请求返回: {}".format(res.text))
