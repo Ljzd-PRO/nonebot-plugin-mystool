41b99cdb2035595d0856d9b3998b9668bfa572d6
Ljzd-PRO <mcdhacg@foxmail.com>
2022-08-27 01:42:49 +0800

    优化文字输出

--- src/nonebot-plugin-mystool/__init__.py
+++ src/nonebot-plugin-mystool/__init__.py
@@ -31,20 +31,20 @@
 from nonebot.plugin import PluginMetadata
 
 __plugin_meta__ = PluginMetadata(
-    name="---米游社小助手插件---\n",
+    name="❖米游社小助手插件❖\n",
     description="米游社工具-每日米游币任务、游戏签到、商品兑换、免抓包登录\n",
     usage="""
-    /登录 -> 跟随指引获取cookie\
-    \n/地址填写 -> 获取地址ID\
-    \n/设置 -> 配置签到、播报相关选项\
-    \n/游戏签到 -> 手动进行米哈游游戏签到\
-    \n/米游社任务 -> 手动进行米游社签到\
-    \n/兑换 -> 进行米游社商品兑换\
-    \n/商品列表 -> 查看米游社当前商品\
-    \n/帮助 -> 查看帮助\
-    \n/帮助 <功能名> -> 查看某一用法具体帮助
+    /登录 ➢ 跟随指引获取cookie\
+    \n/地址 ➢ 获取地址ID\
+    \n/设置 ➢ 配置签到、播报相关选项\
+    \n/签到 ➢ 手动进行米哈游游戏签到\
+    \n/米游社任务 ➢ 手动进行米游社签到\
+    \n/兑换 ➢ 进行米游社商品兑换\
+    \n/商品列表 ➢ 查看米游社当前商品\
+    \n/帮助 ➢ 查看帮助\
+    \n/帮助 <功能名> ➢ 查看某一用法具体帮助
     """.strip(),
-    extra="项目地址：https://github.com/Ljzd-PRO/nonebot-plugin-mysTool\n欢迎提出建议和意见！"
+    extra="🔗项目地址：https://github.com/Ljzd-PRO/nonebot-plugin-mysTool\n欢迎提出建议和意见！"
 )
 
 # 加载其它代码
