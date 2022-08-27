11520d1430d01a35d83babc47524634380964d0e
Ljzd-PRO <mcdhacg@foxmail.com>
2022-08-28 02:40:27 +0800

    修复多账号文字输出问题

--- src/nonebot-plugin-mystool/myb_exchange.py
+++ src/nonebot-plugin-mystool/myb_exchange.py
@@ -54,8 +54,7 @@ async def _(event: PrivateMessageEvent, matcher: Matcher, state: T_State, args=C
             phones = [str(user_account[i].phone)
                       for i in range(len(user_account))]
             msg = "您有多个账号，您要配置以下哪个账号的兑换计划？\n"
-            for ph in phones:
-                msg += '\n'.join("📱 " + ph)
+            msg += "📱" + "\n📱".join(phones)
             await matcher.send(msg)
     # 如果未使用二级命令，则进行查询操作，并结束交互
     else:
