c61cb9c822ebf1a8626618f15bc4fe690246df10
Ljzd-PRO <mcdhacg@foxmail.com>
2022-09-01 16:58:50 +0800

    优化文字输出

--- src/nonebot-plugin-mystool/address.py
+++ src/nonebot-plugin-mystool/address.py
@@ -98,7 +98,9 @@ async def handle_first_receive(event: PrivateMessageEvent, matcher: Matcher, sta
         matcher.set_arg('phone', str(user_account[0].phone))
     else:
         phones = [str(user_account[i].phone) for i in range(len(user_account))]
-        await matcher.send("您有多个账号，您要设置以下哪个账号的地址ID？\n" + '\n'.join(phones))
+        msg = "您有多个账号，您要配置以下哪个账号的兑换计划？\n"
+        msg += "📱" + "\n📱".join(phones)
+        await matcher.send(msg)
 
 
 @get_address.got('phone')
