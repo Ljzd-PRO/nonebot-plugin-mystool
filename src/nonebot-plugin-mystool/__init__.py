36bb769ca9eb43bb2b9df335c76a251bd99cc1bb
Ljzd-PRO <mcdhacg@foxmail.com>
2022-09-03 02:58:49 +0800

    优化文字

--- src/nonebot-plugin-mystool/__init__.py
+++ src/nonebot-plugin-mystool/__init__.py
@@ -21,12 +21,17 @@
 ## 使用说明
 
 ### ⚙️ NoneBot 机器人部署和插件安装
-
 请查看 -> [🔗Installation](https://github.com/Ljzd-PRO/nonebot-plugin-mystool/wiki/Installation)
 
 ### 📖 插件具体使用说明
-
 请查看 -> [🔗Wiki 文档](https://github.com/Ljzd-PRO/nonebot-plugin-mystool/wiki)
+
+### ❓ 获取插件帮助信息
+#### 插件命令
+```
+/帮助
+```
+> ⚠️ 注意 此处没有使用 [🔗 插件命令头](https://github.com/Ljzd-PRO/nonebot-plugin-mystool/wiki/Configuration-Config#command_start)
 """
 
 import pkgutil
@@ -42,17 +47,17 @@ __plugin_meta__ = PluginMetadata(
     name="❖米游社小助手插件❖\nver {}\n".format(VERSION),
     description="米游社工具-每日米游币任务、游戏签到、商品兑换、免抓包登录\n",
     usage="""
-    {HEAD}登录 ➢ 登录绑定米游社账户\
-    \n{HEAD}地址 ➢ 设置收货地址ID
-    \n{HEAD}签到 ➢ 手动进行游戏签到\
-    \n{HEAD}任务 ➢ 手动执行米游币任务
-    \n{HEAD}兑换 ➢ 米游币商品兑换相关\
-    \n{HEAD}商品 ➢ 查看米游币商品信息(商品ID)
-    \n{HEAD}设置 ➢ 设置是否开启通知、每日任务等相关选项\
-    \n{HEAD}账号设置 ➢ 设置是否开启每日米游币任务、游戏签到的计划任务\
-    \n{HEAD}通知设置 ➢ 设置是否开启每日米游币任务、游戏签到的结果通知
-    \n{HEAD}帮助 ➢ 查看帮助信息\
-    \n{HEAD}帮助 <功能名> ➢ 查看目标功能详细说明
+    \n🔐 {HEAD}登录 ➢ 登录绑定米游社账户\
+    \n📦 {HEAD}地址 ➢ 设置收货地址ID
+    \n🖋️ {HEAD}签到 ➢ 手动进行游戏签到\
+    \n🖊️ {HEAD}任务 ➢ 手动执行米游币任务
+    \n🛒 {HEAD}兑换 ➢ 米游币商品兑换相关\
+    \n🎁 {HEAD}商品 ➢ 查看米游币商品信息(商品ID)
+    \n⚙️ {HEAD}设置 ➢ 设置是否开启通知、每日任务等相关选项\
+    \n🔑 {HEAD}账号设置 ➢ 设置设备平台以及是否开启每日计划任务\
+    \n🔔 {HEAD}通知设置 ➢ 设置是否开启每日米游币任务、游戏签到的结果通知
+    \n📖 {HEAD}帮助 ➢ 查看帮助信息\
+    \n🔍 {HEAD}帮助 <功能名> ➢ 查看目标功能详细说明
     """.strip(),
     extra="🔗项目地址：https://github.com/Ljzd-PRO/nonebot-plugin-mystool\n欢迎提出建议和意见！"
 )
