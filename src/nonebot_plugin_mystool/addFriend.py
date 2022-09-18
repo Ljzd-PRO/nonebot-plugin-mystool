e885df7f1f3d659fd264b3869dc61d5d93f2e602
Everything0519 <598139245@qq.com>
2022-09-18 08:58:16 +0800

    增加部分log输出

--- src/nonebot_plugin_mystool/addFriend.py
+++ src/nonebot_plugin_mystool/addFriend.py
@@ -10,6 +10,7 @@ from nonebot_plugin_apscheduler import scheduler
 
 from .config import mysTool_config as conf
 from .data import UserData
+from .utils import logger
 
 driver = get_driver()
 
@@ -22,12 +23,14 @@ async def _(bot: Bot, event: RequestEvent):
     # 判断为加好友事件
     if isinstance(event, FriendRequestEvent):
         if conf.ADD_FRIEND_ACCEPT:
+            logger.info(f'已添加好友{event.user_id}')
             await bot.set_friend_add_request(flag=event.flag, approve=True)
             # 等待腾讯服务器响应
             await asyncio.sleep(1.5)
             await bot.send_private_msg(user_id=event.user_id, message=f'欢迎使用米游社小助手，请发送『{command}帮助』查看更多用法哦~')
     # 判断为邀请进群事件
     elif isinstance(event, GroupRequestEvent):
+        logger.info(f'已添加群聊{event.group_id}')
         # 等待腾讯服务器响应
         await asyncio.sleep(1.5)
         await bot.send_group_msg(group_id=event.group_id, message=f'欢迎使用米游社小助手，请添加小助手为好友后，发送『{command}』帮助 查看更多用法哦~')
@@ -37,13 +40,15 @@ async def check_friend_list():
     """
     检查用户是否仍在好友列表中，不在的话则删除
     """
+    logger.info('正在检查好友列表...')
     bot: Bot = get_bot()
     friend_list = await bot.get_friend_list()
     user_list = UserData.read_all().keys()
     for user in user_list:
         if user not in str(friend_list):
+            logger.info(f'用户{user}不在好友列表内，已删除其数据')
             UserData.del_user(user)
 
 driver.on_bot_connect(check_friend_list)
 scheduler.add_job(id='check_friend', replace_existing=True,
-                  trigger="cron", hour='0', minute='00', func=check_friend_list)
+                  trigger="cron", hour='23', minute='59', func=check_friend_list)
