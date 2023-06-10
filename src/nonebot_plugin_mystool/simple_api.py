15313624d37fcfbe86543593f980d33ef4feaf75
ERZATZ Type AK9 <ljzd@office.ljzd-pro.ml>
2023-06-10 14:26:26 +0800

    Update src/nonebot_plugin_mystool/simple_api.py
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>

--- src/nonebot_plugin_mystool/simple_api.py
+++ src/nonebot_plugin_mystool/simple_api.py
@@ -1091,7 +1091,7 @@ async def get_cookie_token_by_stoken(cookies: BBSCookies, device_id: Optional[st
 
     except tenacity.RetryError as e:
         if is_incorrect_return(e):
-            logger.exception(f"通过 stoken 获取 cookie_token: 服务器没有正确返回")
+            logger.exception("通过 stoken 获取 cookie_token: 服务器没有正确返回")
             logger.debug(f"网络请求返回: {res.text}")
             return GetCookieStatus(incorrect_return=True), None
         else:
