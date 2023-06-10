49a3921e7bc88f2f0283bab63a2338530c99510b
Ljzd-PRO <mcdhacg@foxmail.com>
2023-06-10 14:41:00 +0800

    删除开头注释

--- src/nonebot_plugin_mystool/__init__.py
+++ src/nonebot_plugin_mystool/__init__.py
@@ -1,37 +1,3 @@
-"""
-# mysTool - 米游社辅助工具插件
-
-**版本 - v1.0.0-dev**
-
-## 使用说明
-
-### 🛠️ NoneBot2 机器人部署和插件安装
-
-请查看 -> [🔗Installation](https://github.com/Ljzd-PRO/nonebot-plugin-mystool/wiki/Installation)
-
-### 📖 插件具体使用说明
-
-请查看 -> [🔗Wiki 文档](https://github.com/Ljzd-PRO/nonebot-plugin-mystool/wiki)
-
-### ❓ 获取插件帮助信息
-
-#### 插件命令
-
-```
-/帮助
-```
-
-> ⚠️ 注意 此处没有使用 [🔗 插件命令头](https://github.com/Ljzd-PRO/nonebot-plugin-mystool/wiki/Configuration-Config#command_start)
-
-## 其他
-
-### [📃源码说明](https://github.com/Ljzd-PRO/nonebot-plugin-mystool/wiki/Source-Structure)
-### 适配 [绪山真寻Bot](https://github.com/HibiKier/zhenxun_bot) 的分支
-- https://github.com/MWTJC/zhenxun-plugin-mystool
-- https://github.com/ayakasuki/nonebot-plugin-mystool
-
-"""
-
 import pkgutil
 from pathlib import Path
 
