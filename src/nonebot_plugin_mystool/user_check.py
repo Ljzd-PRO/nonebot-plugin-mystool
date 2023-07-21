eecda9194a504be860254c56022ca70c5fb80c88
Ljzd-PRO <mcdhacg@foxmail.com>
2023-07-22 00:37:49 +0800

    feat: 去掉自动删除好友列表中不存在的用户的功能（频道不需要添加好友）
    Closes #128

--- src/nonebot_plugin_mystool/user_check.py
+++ src/nonebot_plugin_mystool/user_check.py
@@ -2,15 +2,12 @@
 ### QQ好友相关
 """
 import asyncio
-import json
-import os
 
 from nonebot import get_driver, on_request
 from nonebot.adapters.onebot.v11 import (Bot, FriendRequestEvent,
                                          GroupRequestEvent, RequestEvent)
-from nonebot_plugin_apscheduler import scheduler
 
-from .plugin_data import PluginDataManager, write_plugin_data, DELETED_USERS_PATH
+from .plugin_data import PluginDataManager
 from .utils import logger
 
 _conf = PluginDataManager.plugin_data
@@ -34,33 +31,3 @@ async def _(bot: Bot, event: RequestEvent):
     # 判断为邀请进群事件
     elif isinstance(event, GroupRequestEvent):
         logger.info(f'{_conf.preference.log_head}已加入群聊 {event.group_id}')
-
-
-@_driver.on_bot_connect
-async def check_friend_list(bot: Bot):
-    """
-    检查用户是否仍在好友列表中，不在的话则删除
-    """
-    logger.info(f'{_conf.preference.log_head}正在检查好友列表...')
-    friend_list = await bot.get_friend_list()
-    for user in _conf.users:
-        user_filter = filter(lambda x: x["user_id"] == user, friend_list)
-        friend = next(user_filter, None)
-        if not friend:
-            if not os.path.exists(DELETED_USERS_PATH):
-                os.mkdir(DELETED_USERS_PATH)
-            json.dump(
-                _conf.users.pop(user),
-                open(DELETED_USERS_PATH / f"{user}.json", "w"),
-                ensure_ascii=False,
-                indent=4
-            )
-            write_plugin_data()
-            logger.info(f'{_conf.preference.log_head}用户 {user} 不在好友列表内，'
-                        f'已删除其数据，并备份至 {DELETED_USERS_PATH / f"{user}.json"}')
-
-
-@_driver.on_bot_connect
-async def _(bot: Bot):
-    scheduler.add_job(id='check_friend', replace_existing=True,
-                      trigger="cron", hour='23', minute='59', func=check_friend_list, args=(bot,))
