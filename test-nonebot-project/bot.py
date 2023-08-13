e50c436656f43e5489cfe893b74eced04ce6b049
Ljzd-PRO <mcdhacg@foxmail.com>
2023-08-14 01:48:48 +0800

    test: 更改driver

--- test-nonebot-project/bot.py
+++ test-nonebot-project/bot.py
@@ -1,14 +1,8 @@
 import nonebot
-from nonebot.adapters.onebot.v11 import Adapter as OnebotV11
-
-from nonebot.adapters.qqguild import Adapter as QQGuild
 
 nonebot.init()
 
 driver = nonebot.get_driver()
-driver.config.driver = "~fastapi+~httpx+~websockets"
-driver.register_adapter(OnebotV11)
-driver.register_adapter(QQGuild)
 
 nonebot.load_from_toml("pyproject.toml")
 
