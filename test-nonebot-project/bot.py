4e08d10cef78b682b11f3401fa18a06880a5ed8a
Ljzd-PRO <mcdhacg@foxmail.com>
2023-08-14 02:11:32 +0800

    test: 手动加载插件

--- test-nonebot-project/bot.py
+++ test-nonebot-project/bot.py
@@ -5,6 +5,7 @@ nonebot.init()
 driver = nonebot.get_driver()
 
 nonebot.load_from_toml("pyproject.toml")
+nonebot.load_plugins("src/plugins/nonebot_plugin_mystool")
 
 if __name__ == "__main__":
     nonebot.run()
