d9dbe90ff616a1121f85a296e5369a41c0f94f9d
Ljzd-PRO <mcdhacg@foxmail.com>
2023-08-14 02:19:09 +0800

    test: 删除重复导入的插件

--- test-nonebot-project/bot.py
+++ test-nonebot-project/bot.py
@@ -5,7 +5,6 @@ nonebot.init()
 driver = nonebot.get_driver()
 
 nonebot.load_from_toml("pyproject.toml")
-nonebot.load_plugins("src/plugins/nonebot_plugin_mystool")
 
 if __name__ == "__main__":
     nonebot.run()
